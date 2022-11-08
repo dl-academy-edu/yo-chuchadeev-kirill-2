b63cded6d5d007e0b7cf497a0f286b8083a91f59
Kirill <beerbox7@yandex.com>
2022-11-08 21:17:53 +0400

    dlk
    Сверстал грид, не доделал еще адаптив. Вопрос как сделать чтобы картинки сжимались? и в гридах и в свг и в анимации

--- source/js/index.js
+++ source/js/index.js
@@ -1,6 +1,7 @@
 var popup = document.querySelector('.popup')
 var popapBtn = document.querySelector('.index__banner_js')
 var popapClose = document.querySelector('.popap__close')
+
 var check = document.querySelector ('#popup__input__check')
 var submit = document.querySelector ('.popup__button__submit')
 submit.setAttribute('disabled', true)
