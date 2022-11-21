1922a151bb4ca4ab4bc401ada9250432407932ea
Kirill <beerbox7@yandex.com>
2022-11-21 21:47:18 +0400

    DLK 2
    Доделал адаптацию, кроме страницы анимации и свг. (?)
    Доделал бургер меню.
    Сделал слайдер на индексе.
    Добавил аудио на анимации.
    Можно проверить

--- source/js/header_menu.js
+++ source/js/header_menu.js
@@ -1,16 +1,19 @@
 var header = document.querySelector('.header__nav')
 var burgerMenuOpen = document.querySelector('.burger_menu_open_js')
 var burgerMenuClose = document.querySelector('.burger_menu_close_js')
+var body = document.querySelector('#body')
 
 
 burgerMenuOpen.addEventListener('click', function() {
-    header.classList.add('header__nav_open');
-    document.getElementById("burger_menu_open_js").style.display = "none";
-    document.getElementById("burger_menu_close_js").style.display = "flex";
+    header.classList.toggle('header__nav_open')
+    burgerMenuOpen.classList.add('menu_close')
+    burgerMenuClose.classList.add('menu_open')
+    body.classList.add('body_lock')
 })
 
 burgerMenuClose.addEventListener('click', function() {
-    header.classList.remove('header__nav_open');
-    document.getElementById("burger_menu_open_js").style.display = "flex";
-    document.getElementById("burger_menu_close_js").style.display = "none";
+    header.classList.toggle('header__nav_open')
+    burgerMenuOpen.classList.remove('menu_close')
+    burgerMenuClose.classList.remove('menu_open')
+    body.classList.remove('body_lock')
 })
--- source/js/index.js
+++ source/js/index.js
@@ -1,10 +1,12 @@
 var popup = document.querySelector('.popup')
 var popapBtn = document.querySelector('.index__banner_js')
 var popapClose = document.querySelector('.popup__close')
+var body = document.querySelector('#body')
 
 var check = document.querySelector ('#popup__input__check')
 var submit = document.querySelector ('.popup__button__submit')
 submit.setAttribute('disabled', true)
+
 
 check.oninput = function() {
     if (check.checked) {
@@ -16,8 +18,32 @@ check.oninput = function() {
 
 popapBtn.addEventListener('click', function() {
     popup.classList.add('popup_open')
+    body.classList.add('body_lock')
 })
 
 popapClose.addEventListener('click', function() {
     popup.classList.remove('popup_open')
+    body.classList.remove('body_lock')
 })
+
+
+// slider
+
+let offset = 0;
+const slider = document.querySelector('.category__products__list');
+
+document.querySelector('.slider_right').addEventListener('click', function(){
+    offset = offset + 300;
+    if (offset > 600) {
+        offset = 0;
+    }
+    slider.style.left = -offset + 'px';
+});
+
+document.querySelector('.slider_left').addEventListener('click', function(){
+    offset = offset - 300;
+    if (offset < 0) {
+        offset = 600;
+    }
+    slider.style.left = -offset + 'px';
+});
